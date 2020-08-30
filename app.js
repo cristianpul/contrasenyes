aa5f2bc4a634e707bfae0a42b1af2fc92d6012aa
Cristian Pulido <cristianpul@gmail.com>
2020-08-30 09:12:30 +0200

    Adaptació ruta per web

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
-/**
- * app.js
- */
-
-// Carregar mòduls de Node.js
 var express = require("express");
 var bodyParser = require("body-parser");
 
-// Executar Express (http)
 var app = express();
 
-// Carregar fitxer de rutes
 var pass_rutes = require("./routes/pass_rutes");
+var path = require("path");
 
-// Carregar middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Afegir prefixes a rutes i carregar rutes
+app.use("/", express.static(path.join(__dirname, "/web")));
 app.use("/api", pass_rutes);
 
-// Activar el CORS
-
-// Exportar mòdul (arxiu actual)
 module.exports = app;
