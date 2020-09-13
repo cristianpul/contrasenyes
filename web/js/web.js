7b3c3943eab23941f4273bf355e28ee84bcaff66
Cristian Pulido <cristianpul@gmail.com>
2020-09-13 13:09:56 +0200

    Canvis de format

--- web/js/web.js
+++ web/js/web.js
@@ -1,28 +1,28 @@
 function caracteristiques() {
-  let llargada = document.getElementById("llargada").value;
-  let minuscules = document.getElementById("minuscules").checked;
-  let majuscules = document.getElementById("majuscules").checked;
-  let numeros = document.getElementById("numeros").checked;
-  let especials = document.getElementById("especials").checked;
-  let evitaSimilars = document.getElementById("evita-similars").checked;
+  let llargada = document.getElementById('llargada').value;
+  let minuscules = document.getElementById('minuscules').checked;
+  let majuscules = document.getElementById('majuscules').checked;
+  let numeros = document.getElementById('numeros').checked;
+  let especials = document.getElementById('especials').checked;
+  let evitaSimilars = document.getElementById('evita-similars').checked;
 
-  let parametres = peticioInicial + "/" + llargada;
+  let parametres = peticioInicial + '/' + llargada;
   if (minuscules || majuscules || numeros || especials || evitaSimilars) {
-    parametres += "/p=";
+    parametres += '/p=';
     if (minuscules) {
-      parametres += "minuscules;";
+      parametres += 'minuscules;';
     }
     if (majuscules) {
-      parametres += "majuscules;";
+      parametres += 'majuscules;';
     }
     if (numeros) {
-      parametres += "numeros;";
+      parametres += 'numeros;';
     }
     if (especials) {
-      parametres += "caractersEspecials;";
+      parametres += 'caractersEspecials;';
     }
     if (evitaSimilars) {
-      parametres += "evitaSimilars;";
+      parametres += 'evitaSimilars;';
     }
     parametres = parametres.slice(0, -1);
   }
@@ -30,38 +30,34 @@ function caracteristiques() {
 }
 
 function actualitzaContrasenya(peticio) {
-  peticioActualitza.open("GET", peticio);
-  peticioActualitza.responseType = "json";
+  peticioActualitza.open('GET', peticio);
+  peticioActualitza.responseType = 'json';
   peticioActualitza.send();
 
   peticioActualitza.onload = function () {
     let contrasenyaJSON = peticioActualitza.response;
     console.log(contrasenyaJSON.contrasenya);
-    document.getElementById("contrasenya").value = contrasenyaJSON.contrasenya;
+    document.getElementById('contrasenya').value = contrasenyaJSON.contrasenya;
   };
 }
 
 function copiaContrasenya() {
-  let contrasenyaPerCopiar = document.getElementById("contrasenya");
+  let contrasenyaPerCopiar = document.getElementById('contrasenya');
 
   contrasenyaPerCopiar.select();
   contrasenyaPerCopiar.setSelectionRange(0, 99999); /*For mobile devices*/
-  document.execCommand("copy");
+  document.execCommand('copy');
 
-  let tooltip = document.getElementById("copiaTooltip");
-  tooltip.innerHTML = "Copiada 🚀";
-}
-
-function regeneraContrasenya() {
-  console.log("Regenera!");
+  let tooltip = document.getElementById('copiaTooltip');
+  tooltip.innerHTML = 'Copiada 🚀';
 }
 
 function foraTooltip(element) {
-  let tooltip = document.getElementById("copiaTooltip");
+  let tooltip = document.getElementById('copiaTooltip');
 
-  if (element === "copiaTooltip") {
-    tooltip.innerHTML = "Copia";
+  if (element === 'copiaTooltip') {
+    tooltip.innerHTML = 'Copia';
   } else {
-    tooltip.innerHTML = "Regenera";
+    tooltip.innerHTML = 'Regenera';
   }
 }
